8af29d23746c69228161cabee9ec99da6eef23ee
vtorres789 <victortorres180@gmail.com>
2023-07-24 19:45:25 -0400

    workshop edit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,12 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import ContactList from "./components/contactList";
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
 
